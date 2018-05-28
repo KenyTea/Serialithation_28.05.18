5d0f5ee947bd598cca4e4c1fc43f870fe461d7c4
Васильева <@223_16>
2018-05-28 19:37:19 +0600

    Add project files.

---
+++ Serialithation_28.05.18/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialithation_28._05._18
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+
+    public class Human : Person
+    {
+        public Human(string name, int age) : base(name, age)
+        {
+        }
+    }
+}
---
+++ Serialithation_28.05.18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary; // !!!!!!!!!!!!!!!!!!!!!!!
+using System.IO;
+
+namespace Serialithation_28._05._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Kim", 30);
+            Console.WriteLine("Object create!");
+
+            // Сщздаём объект BinaryFormatter
+            BinaryFormatter format = new BinaryFormatter();
+            using (FileStream fs = new FileStream("person.dat", FileMode.OpenOrCreate))
+            {
+                format.Serialize(fs, person);
+                Console.WriteLine("Object serialzed!");
+
+            }
+        }
+    }
+}
